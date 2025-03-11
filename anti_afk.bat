de28d53e023d520ce13a82599e324d2c9844cd7c
Aloysius Goh <aloysiusgohkw@gmail.com>
2025-03-12 00:38:04 +0800

    first commit

---
+++ anti_afk.bat
@@ -0,0 +1,5 @@
+@echo off
+
+start /min cmd /k "python C:\Users\65878\Downloads\Programming\Usable\AHK\anti_afk\anti_afk.py"
+
+exit
---
+++ anti_afk.py
@@ -0,0 +1,48 @@
+import time
+import pygetwindow as gw
+import pyautogui
+
+WINDOW_TITLE = "Roblox"
+
+def bring_window_to_front():
+    try:
+        pos_x, pos_y = pyautogui.position()
+        sleep_time = 0.1
+        window = gw.getWindowsWithTitle(WINDOW_TITLE)
+        if window:
+            win = window[0]
+            if win.isMinimized:
+                win.restore()
+            win.activate()
+            print(f"Brought '{WINDOW_TITLE}' to front.")
+            time.sleep(sleep_time)
+            
+            offset = 65
+            pyautogui.moveTo(win.left + offset, win.top + offset)    
+            pyautogui.mouseDown(button='right')
+            time.sleep(sleep_time)
+            pyautogui.mouseUp(button='right')
+            time.sleep(sleep_time)
+            pyautogui.click()
+            time.sleep(sleep_time)
+            pyautogui.mouseDown(button='right')
+            time.sleep(sleep_time)
+            pyautogui.mouseUp(button='right')
+            time.sleep(sleep_time)
+            pyautogui.click()
+            print(f"Clicked '{WINDOW_TITLE}'.")
+            time.sleep(sleep_time * 2)
+            
+            win.minimize()
+            print(f"Minimised '{WINDOW_TITLE}'.")
+            
+            pyautogui.moveTo(pos_x, pos_y)
+        else:
+            print(f"Window '{WINDOW_TITLE}' not found.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    while True:
+        bring_window_to_front()
+        time.sleep(600)
